ac870f01d9c3b7f4df6515b8ecfdda79ddac5b13
Matt Corallo <coingenbot@bluematt.me>
2014-04-03 14:43:18 +0200

    Change block rate/subsidy/block value

--- src/main.cpp
+++ src/main.cpp
@@ -1075,8 +1075,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 50 * COIN;
 
-    // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 210000);
+    // Subsidy is cut in half every 8200000 blocks, which will occur approximately every 4 years
+    nSubsidy >>= (nHeight / 8200000);
 
     return nSubsidy + nFees;
 }
